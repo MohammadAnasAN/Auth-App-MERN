ae7293d4a2b7bad4d205e4725d0bcf8bd752e0b5
MohammadAnasAN <mohammadanasan@gmail.com>
2024-07-24 18:23:26 +0530

    profile pic updation option addded and profile is setted

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,10 +4,10 @@ import { useSelector } from "react-redux";
 export default function Header() {
     const { currentUser } = useSelector((state) => state.user);
   return (
-    <div className='bg-slate-200'>
+    <div className='bg-blue-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3' >
             <Link to='/'>
-            <h1 className='font-bold'>Auth App</h1>
+            <h1 className='font-bold'>MERN APP</h1>
             </Link>
             <ul className='flex gap-4'>
                 <Link to='/' ><li>Home</li></Link>
